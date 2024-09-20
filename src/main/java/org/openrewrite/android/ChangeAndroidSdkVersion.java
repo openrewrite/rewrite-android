c55b554175970bdf99b514c0c35a78b6016e8458
Tim te Beek <tim@moderne.io>
2024-09-20 20:03:25 +0200

    Change Android SDK version to argument version (#2)
    * Change Android SDK version to argument version
    
    * Minimize indentation
    
    * Only operate on build.gradle files

---
+++ src/main/java/org/openrewrite/android/ChangeAndroidSdkVersion.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.android;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.gradle.IsBuildGradle;
+import org.openrewrite.groovy.GroovyIsoVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.tree.J;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class ChangeAndroidSdkVersion extends Recipe {
+
+    @Option(displayName = "Android SDK Version",
+            description = "The version of the Android SDK to use.",
+            example = "35")
+    Integer version;
+
+    @Override
+    public String getDisplayName() {
+        return "Change Android SDK version";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Change `compileSdk`, `compileSdkVersion`, `targetSdk` and `targetSdkVersion` in an Android Gradle build file to the argument version.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new IsBuildGradle<>(), new GroovyIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                J.MethodInvocation mi = super.visitMethodInvocation(method, ctx);
+                String simpleName = mi.getSimpleName();
+                if (simpleName.equals("compileSdk") || simpleName.equals("targetSdk")) {
+                    return mi.withArguments(ListUtils.map(mi.getArguments(), arg -> {
+                        if (arg instanceof J.Literal && !version.equals(((J.Literal) arg).getValue())) {
+                            return ((J.Literal) arg).withValue(version).withValueSource(String.valueOf(version));
+                        }
+                        return arg;
+                    }));
+                }
+                if (simpleName.equals("compileSdkVersion") || simpleName.equals("targetSdkVersion")) {
+                    return mi.withArguments(ListUtils.map(mi.getArguments(), arg -> {
+                        if (arg instanceof J.Literal && !String.valueOf(version).equals(((J.Literal) arg).getValue())) {
+                            return ((J.Literal) arg).withValue(String.valueOf(version)).withValueSource(String.format("\"%d\"", version));
+                        }
+                        return arg;
+                    }));
+                }
+                return mi;
+            }
+        });
+    }
+}
---
+++ src/test/java/org/openrewrite/android/ChangeAndroidSdkVersionTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.android;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
+
+class ChangeAndroidSdkVersionTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new ChangeAndroidSdkVersion(35))
+          .beforeRecipe(withToolingApi("8.10.1", "bin"));
+    }
+
+    @DocumentExample
+    @Test
+    void agp7OrHigher() {
+        rewriteRun(
+          //language=gradle
+          buildGradle(
+            """
+              buildscript {
+                  repositories {
+                      mavenCentral()
+                      google()
+                  }
+                  dependencies {
+                      classpath 'com.android.tools.build:gradle:8.6.1'
+                  }
+              }
+              repositories {
+                  mavenCentral()
+              }
+              apply plugin: 'com.android.application'
+              group = "org.example"
+              version = "1.0-SNAPSHOT"
+              android {
+                 compileSdk 28
+                 defaultConfig {
+                     targetSdk 28
+                 }
+                 namespace = "com.example.myapp"
+              }
+              """,
+            """
+              buildscript {
+                  repositories {
+                      mavenCentral()
+                      google()
+                  }
+                  dependencies {
+                      classpath 'com.android.tools.build:gradle:8.6.1'
+                  }
+              }
+              repositories {
+                  mavenCentral()
+              }
+              apply plugin: 'com.android.application'
+              group = "org.example"
+              version = "1.0-SNAPSHOT"
+              android {
+                 compileSdk 35
+                 defaultConfig {
+                     targetSdk 35
+                 }
+                 namespace = "com.example.myapp"
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void agp4OrLower() {
+        rewriteRun(
+          //language=gradle
+          buildGradle(
+            """
+              buildscript {
+                  repositories {
+                      mavenCentral()
+                      google()
+                  }
+                  dependencies {
+                      classpath 'com.android.tools.build:gradle:4.1.3'
+                  }
+              }
+              repositories {
+                  mavenCentral()
+              }
+              apply plugin: 'com.android.application'
+              group = "org.example"
+              version = "1.0-SNAPSHOT"
+              android {
+                 compileSdkVersion 28
+                 defaultConfig {
+                     targetSdkVersion 28
+                 }
+              }
+              """,
+            """
+              buildscript {
+                  repositories {
+                      mavenCentral()
+                      google()
+                  }
+                  dependencies {
+                      classpath 'com.android.tools.build:gradle:4.1.3'
+                  }
+              }
+              repositories {
+                  mavenCentral()
+              }
+              apply plugin: 'com.android.application'
+              group = "org.example"
+              version = "1.0-SNAPSHOT"
+              android {
+                 compileSdkVersion "35"
+                 defaultConfig {
+                     targetSdkVersion "35"
+                 }
+              }
+              """
+          )
+        );
+    }
+}
