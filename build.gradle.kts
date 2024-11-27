b546397a2bf27381580d5c61c24fea382b6f795b
Tim te Beek <tim@moderne.io>
2024-11-27 16:18:56 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,11 @@ description = "Rewrite Android recipes."
 val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
     implementation(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
-    implementation("org.openrewrite:rewrite-gradle:$rewriteVersion")
+    implementation("org.openrewrite:rewrite-gradle")
 
     runtimeOnly("org.openrewrite:rewrite-java-17")
 
+    testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.openrewrite.gradle.tooling:model:${rewriteVersion}")
     testImplementation(gradleApi())
 }
